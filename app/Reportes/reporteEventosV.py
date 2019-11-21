5afff682e47380e6db27de2ec443f6d47391c9f5
Rodrigo Roldan <roldan096@gmail.com>
2019-11-21 13:15:42 -0600

    correccion horarios reportes

--- app/Reportes/reporteEventosV.py
+++ app/Reportes/reporteEventosV.py
@@ -484,9 +484,9 @@ def main (argv):
     horario_inicio = ""
     horario_fin = ""
 
-    horario_matutino = "07:00:00"
-    horario_vespertino = "15:00:00"
-    horario_nocturno = "23:00:00"
+    horario_matutino = "06:30:00"
+    horario_vespertino = "14:30:00"
+    horario_nocturno = "22:30:00"
 
     #fecha_hoy = str(date.today())
     fecha_ayer = str(datetime.strptime(fecha_hoy,"%Y-%m-%d").date() - timedelta(days=1))
